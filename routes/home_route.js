098bbe3f5f28776b8c02e146b41c667c84fd58bf
Vladimir Chernya <chernyla@hotmail.com>
2016-11-22 00:05:36 -0500

    updated home_route.js

--- routes/home_route.js
+++ routes/home_route.js
@@ -1,9 +1,8 @@
 'use strict'
 const Router = require('express').Router();
-const {getMovies}  = require('../models/movies_model');
-const {insertMovie}  = require('../models/movies_model');
-const {deleteMovie}  = require('../models/movies_model');
-const {getMovieInfo} = require('../services/find_movie_api');
+const movieDB = require('../models/movies_model');
+
+const movieExternalApi = require('../services/find_movie_api');
 const path = require('path');
 // const {getApartments}  = require('../models/apartment_model');
 
@@ -16,33 +15,33 @@ const sendJSONresp = (req, res) => res.json(res.returnRecords);
 
 // To show the landing page
 Router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../public/landing.html'));
+    res.sendFile(path.join(__dirname, '/../public/landing.html'));
 
 });
 
 // To show the details page
 Router.get('/details', (req, res) => {
-  res.render('details')
+    res.render('details')
 });
 
 
 Router.route('/api/movies')
-// To get all movies from the database
- .get(getMovies, sendJSONresp)
-// To update notes for a single movie
- .post(insertMovie, sendJSONresp);
+    // To get all movies from the database
+    .get(movieDB.getMovies, sendJSONresp)
+    // To update notes for a single movie
+    .post(movieDB.insertMovie, sendJSONresp);
 
 Router.route('/api/movies/:id')
-// To get single movie by id
- .get(getMovies, sendJSONresp)
- // To delete entire movie from the database
- .delete(deleteMovie, sendJSONresp);
+    // To get single movie by id
+    .get(movieDB.getMovies, sendJSONresp)
+    // To delete entire movie from the database
+    .delete(movieDB.deleteMovie, sendJSONresp);
 
 
 Router.route('/api/movieinfo')
-// Request to external api
-.get(getMovieInfo, sendJSONresp)
- 
+    // Request to external api
+    .get(movieExternalApi.getMovieInfo, sendJSONresp)
+
 
 
 
